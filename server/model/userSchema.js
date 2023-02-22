b8f42f6f72bbf08c16ea832c335c76c37fb5241b
“Himanshu” <agarkarhimanshu456@gmail.com>
2023-02-22 08:28:24 +0530

    bcrypt hashing, login, signin done

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,32 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true
-    },
-    work: {
-        type: String,
-        require: true
-    },
-    phone: {
-        type: Number,
-        require: true
-    },
-    password: {
-        type: String,
-        require: true
-    },
-    cpassword: {
-        type: String,
-        require: true
-    },
-})
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  work: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: Number,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  cpassword: {
+    type: String,
+    require: true,
+  },
+});
 
-const User = mongoose.model('USER', userSchema) //User is what is in collection - users
+// we are hashing the password
+
+userSchema.pre("save", async function (next) {
+  // when we need this keyword, it works totally opposite when we use arrow function, this function returns promises, so we make it async
+  console.log("inside")
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+});
+
+const User = mongoose.model("USER", userSchema); //User is what is in collection - users
 
 module.exports = User;
